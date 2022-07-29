0471e55117e7974237e65f7df0dd300cd4331086
Arjun Kadam <arjunkadama29@gmail.com>
2022-07-29 15:43:37 +0530

    Addition Of Arguments File Updated

--- src/com/company/AvgOfArguments.java
+++ src/com/company/AvgOfArguments.java
@@ -1,15 +1,19 @@
 package com.company;
 
 public class AvgOfArguments {
-    static void avg(int...arr) {
+    static int avg(int...arr) {
         int res = 0;
         int length= arr.length;
         for (int i = 0; i < length; i++) {
             res +=arr[i];
         }
-        System.out.println(res);
+        //System.out.println(res);
+        return res;
     }
     public static void main(String[] args) {
-        avg(10,2,2,1);
+
+        int res=avg(10,2,2,1);
+        System.out.println(res);
+
     }
 }
